e1de69f1c9e320e79142d7a6b904cb7ab5d336a5
nehachatse <nehachatse02@gmail.com>
2022-01-23 12:58:31 +0530

    middleware

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -7,4 +7,15 @@ const rootReducer = combineReducers({
     app: appReducer
 });
 
-export const store = createStore(rootReducer);
+const loggerMiddleware = (store) => (next) => action => {
+    console.log("dispatching the action", action);
+    console.log(store.getState);
+    const value = next(action);
+    console.log(value);
+    console.log(store.getState);
+}
+
+export const store = createStore(rootReducer, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(loggerMiddleware)
+    );
